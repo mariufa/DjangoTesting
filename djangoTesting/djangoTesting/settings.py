b92b854315318ea69615d91e3b492fcd666f3183
Marius Fagerland <mariusfagerland@hotmail.com>
2016-06-14 23:38:20 +0200

    Django project created

--- djangoTesting/djangoTesting/settings.py
+++ djangoTesting/djangoTesting/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'athcaq5f&ijd23gjy=u2!!-1^4mi4-*qbp-ngcak=do=s96co*'
+with open('secret_key.txt') as secretFile:
+    SECRET_KEY = secretFile.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
