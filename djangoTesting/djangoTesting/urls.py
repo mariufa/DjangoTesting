b4df43976e504c0a5e0b10ef37688cc8e10a3ba1
Marius Fagerland <mariusfagerland@yahoo.no>
2016-07-04 18:24:25 +0200

    Setup projects url

--- djangoTesting/djangoTesting/urls.py
+++ djangoTesting/djangoTesting/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('frontPage.urls', namespace="frontPage")),
+    url(r'^projects/', include('projects.urls')),
     url(r'^admin/', admin.site.urls),
 ]
