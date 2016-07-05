ed1d539b53b016b11fada92473409e5229211ae6
Marius Fagerland <mariusfagerland@yahoo.no>
2016-07-05 18:54:40 +0200

    Setup projects index.html with base_index.html

--- djangoTesting/projects/views.py
+++ djangoTesting/projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("My projects")
+    template = loader.get_template('projects/index.html')
+    return HttpResponse(template.render(request))
