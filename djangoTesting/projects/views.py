ff8469954aa850c30164a3ab279e637203023dbe
Marius Fagerland <mariusfagerland@yahoo.no>
2016-07-06 17:08:17 +0200

    Add context to view

--- djangoTesting/projects/views.py
+++ djangoTesting/projects/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Project
+
 def index(request):
+    projectsList = Project.objects.all()
     template = loader.get_template('projects/index.html')
-    return HttpResponse(template.render(request))
+    context = {
+        'projectsList': projectsList,
+    }
+    return HttpResponse(template.render(context, request))
